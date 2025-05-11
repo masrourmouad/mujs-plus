6654464d51f949f332ba17f0382c19911bd1ab17
masrourmouad <dbliss3@proton.me>
2025-05-11 13:17:01 +0100

    Update jsmath.c

--- jsmath.c
+++ jsmath.c
@@ -133,7 +133,7 @@ static void Math_rand(js_State *J)
 
 static void Math_srand(js_State *J)
 {
-	unsigned int seed = js_isdefined(J, 1) ? js_tonumber(J, 1) : 0;
+	int seed = js_isdefined(J, 1) ? js_tonumber(J, 1) : 0;
 	
 	if(seed > 0)
 	{
